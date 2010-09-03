d12384f886c06bd58155d9b15e0bcceb420522cd
Sascha Kiefer <sk@intertivity.com>
2010-09-03 20:08:34 +0400

    cleanup.

--- lib/Mojolicious/Plugin/Cms/Store/Cache.pm
+++ lib/Mojolicious/Plugin/Cms/Store/Cache.pm
@@ -4,36 +4,35 @@ use base 'Mojolicious::Plugin::Cms::Store';
 use strict;
 use warnings;
 
-__PACKAGE__->attr(cache => sub { $_[0]->cms->cache });	
+__PACKAGE__->attr(cache => sub { $_[0]->cms->cache });
 __PACKAGE__->attr(store => sub { $_[0]->cms->store });
 
 sub _retrieve {
-	my $self = shift;
-	my $method = shift;
-	my $set_it = shift;
+    my $self   = shift;
+    my $method = shift;
+    my $set_it = shift;
 
-	my $id = join '.', grep {$_} @_;	
-	my $rc = $self->cache->get( $id );
-	unless(defined $rc)
-	{
-		$rc = $self->store->$method( @_ );
-		$self->cache->set($id, $rc) if $set_it && defined $rc;
-	}
-	return $rc;
+    my $id = join '.', grep {$_} @_;
+    my $rc = $self->cache->get($id);
+    unless (defined $rc) {
+        $rc = $self->store->$method(@_);
+        $self->cache->set($id, $rc) if $set_it && defined $rc;
+    }
+    return $rc;
 }
 
 sub _get {
-	my $self = shift;
-	my $method = shift;
-	
-	return $self->_retrieve( $method, 1, @_ );
+    my $self   = shift;
+    my $method = shift;
+
+    return $self->_retrieve($method, 1, @_);
 }
 
 sub _get_only {
-	my $self = shift;
-	my $method = shift;
-	
-	return $self->_retrieve( $method, 0, @_ );
+    my $self   = shift;
+    my $method = shift;
+
+    return $self->_retrieve($method, 0, @_);
 }
 
 # backup to the store only, don't cache it
@@ -41,17 +40,17 @@ sub backup { shift->store->backup(@_) }
 
 sub exists {
     my $self = shift;
-	return $self->_get_only(exists => @_);
+    return $self->_get_only(exists => @_);
 }
 
 sub list {
     my $self = shift;
-	return $self->store->list(@_);
+    return $self->store->list(@_);
 }
 
 sub list_by_category {
     my $self = shift;
-	return $self->_get(list_by_category => @_);
+    return $self->_get(list_by_category => @_);
 }
 
 sub list_by_tag {
@@ -61,7 +60,7 @@ sub list_by_tag {
 
 sub load {
     my $self = shift;
-	return $self->_get(load => @_);    
+    return $self->_get(load => @_);
 }
 
 # restore from the store only, not from the cache
