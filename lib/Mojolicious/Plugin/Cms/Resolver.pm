db1a0074db203273dc76f4de6df58e4301c2994b
unknown <Sascha Kiefer@.(none)>
2010-09-20 23:29:09 +0400

    added a littel Resolver POD

--- lib/Mojolicious/Plugin/Cms/Resolver.pm
+++ lib/Mojolicious/Plugin/Cms/Resolver.pm
@@ -71,7 +71,7 @@ __END__
 
 =head1 NAME
 
-Mojolicious::Plugin::Cms::Resolver - Resolves server bindings inside XML
+Mojolicious::Plugin::Cms::Resolver - Adds dynamic spice to your CMS content
 
 =head1 SYNOPSIS
 
@@ -87,6 +87,58 @@ Mojolicious::Plugin::Cms::Resolver - Resolves server bindings inside XML
 	$resolved = $resolver->resolve( $controller, '<span runat="server"><%= $foo %></span>');
 	
 =head1 DESCRIPTION
+
+Using the resolver your able to add more dynamic features to your CMS content.
+CMS content by it self is not dynamic, you write your text or markup, that get's translated to (X)HTML, 
+and is served to the web client. You dont't have any dynamic features you have when using templates - 
+and you do not want that, because content writers usually don't have any knowledge about writing code.
+Use the resolver to spice up your content.
+
+=head1 EXAMPLES
+
+=head2 Current time
+
+One simple example is too insert the current time. So you define your foo.en.cms and add the following 
+markdown text.
+
+	Current Time
+	============
+	
+	This content was served at: <time />
+
+On the server side you add the following to your resolver
+
+	$resolver->bind(time => sub { time });
+	
+When the content is rendered, the <time /> tag will be replaced with the value of time.
+HINT: The resolver already defines this binding, so you do not have to do it again.
+
+=head2 Twitter search
+
+Display twitter keyword results is easy. Write some markdown
+
+	Twitter results for Mojolicious
+	===============================
+	
+	<twitter serach="mojolicious" />
+	
+A simple resolver code looks like this
+
+	use Mojo::Client;
+	my $client = Mojo::Client->new;
+	$resolver->bind(twitter => sub {
+		my $entity = shift;
+		
+		return 'No search key defined' unless my $search = $entity->attrs->{search};
+		
+		my $retval = '';
+		my $results = $client->get("http://search.twitter.com/search.json?q=$search")->res->json->{results};
+		foreach my $r (@$results) {		
+			$retval .= $r->{from_user} . ' wrote:' . $r->{text} . "<br />\n";
+		}
+		
+		return $retval;
+	});
 
 =cut
 
